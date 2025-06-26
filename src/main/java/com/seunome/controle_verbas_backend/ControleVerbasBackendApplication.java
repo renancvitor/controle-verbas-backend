736835c70a0fb699cfe48e967266207657405dc3
Renan C. Vitor <renan.vitor.cm@gmail.com>
2025-06-26 12:39:51 -0300

    feat(api): adiciona projeto Spring Boot inicial com dependências e estrutura básica

---
+++ src/main/java/com/seunome/controle_verbas_backend/ControleVerbasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seunome.controle_verbas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleVerbasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleVerbasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seunome/controle_verbas_backend/ControleVerbasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seunome.controle_verbas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleVerbasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
