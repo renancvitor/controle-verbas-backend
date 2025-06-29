727d2a830cb8b7d012434879de7391d7cc4e7c28
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-06-28 23:56:54 -0300

    feat(departamento): adiciona serviço

--- src/main/java/com/api/controleverbasbackend/service/DepartamentoService.java
+++ src/main/java/com/api/controleverbasbackend/service/DepartamentoService.java
@@ -1,5 +1,30 @@
 package com.api.controleverbasbackend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.api.controleverbasbackend.domain.departamento.Departamento;
+import com.api.controleverbasbackend.dto.departamento.DadosDetalhamentoDepartamento;
+import com.api.controleverbasbackend.dto.departamento.DadosListagemDepartamento;
+import com.api.controleverbasbackend.repository.DepartamentoRepository;
+
+@Service
 public class DepartamentoService {
 
+    @Autowired
+    private DepartamentoRepository departamentoRepository;
+
+    @Transactional
+    public Page<DadosListagemDepartamento> listar(Pageable pageable) {
+        return departamentoRepository.findAll(pageable).map(DadosListagemDepartamento::new);
+    }
+
+    @Transactional
+    public DadosDetalhamentoDepartamento cadastrar(DadosDetalhamentoDepartamento dados) {
+        Departamento departamento = new Departamento();
+        return new DadosDetalhamentoDepartamento(departamento);
+    }
 }
