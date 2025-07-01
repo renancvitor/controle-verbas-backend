f6bf7e42295591467c8b0a56524d2678cfb74467
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-01 18:23:56 -0300

    feat(usuario): adiciona repositório

---
+++ src/main/java/com/api/controleverbasbackend/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.api.controleverbasbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.controleverbasbackend.domain.usuario.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
