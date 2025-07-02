7fd9361cf0ee0083f378e2710fb4c009e879c31e
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-02 18:16:14 -0300

    feat(auth): adiciona controlador de autenticação

---
+++ src/main/java/com/api/controleverbasbackend/controller/AutenticacaoController.java
@@ -0,0 +1,32 @@
+package com.api.controleverbasbackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.controleverbasbackend.dto.autenticacao.DadosLogin;
+import com.api.controleverbasbackend.dto.autenticacao.DadosTokenJWT;
+import com.api.controleverbasbackend.service.AutenticacaoService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
+    @PostMapping
+    public ResponseEntity autenticacao(@RequestBody @Valid DadosLogin dados) {
+        String tokenJwt = autenticacaoService.autenticacao(dados, authenticationManager);
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJwt));
+    }
+}
