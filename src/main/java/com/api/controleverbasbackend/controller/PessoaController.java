9313b888e2c22a2842b2223c8608f2e187709c92
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-07 17:46:23 -0300

    feat(pessoa): adiciona recurso para ativar pessoa na controladora

--- src/main/java/com/api/controleverbasbackend/controller/PessoaController.java
+++ src/main/java/com/api/controleverbasbackend/controller/PessoaController.java
@@ -67,4 +67,10 @@ public class PessoaController {
         pessoaService.deletar(id, usuario);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping("/{id}/ativar")
+    public ResponseEntity<Void> ativar(@PathVariable Long id, @AuthenticationPrincipal Usuario usuario) {
+        pessoaService.ativar(id, usuario);
+        return ResponseEntity.noContent().build();
+    }
 }
