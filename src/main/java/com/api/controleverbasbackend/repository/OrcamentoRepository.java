f3ec5d54bdace4897d07bb598d6be75ffff56ab5
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-09 12:59:21 -0300

    feat(orcamento): adiciona listagem ao serviço

--- src/main/java/com/api/controleverbasbackend/repository/OrcamentoRepository.java
+++ src/main/java/com/api/controleverbasbackend/repository/OrcamentoRepository.java
@@ -1,8 +1,29 @@
 package com.api.controleverbasbackend.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.api.controleverbasbackend.domain.orcamento.Orcamento;
 
 public interface OrcamentoRepository extends JpaRepository<Orcamento, Long> {
+
+    @Query("""
+                SELECT o FROM Orcamento o
+                WHERE
+                    o.statusOrcamentoEntidade.id = :aprovadoId
+                    OR (o.solicitante.id = :idUsuario AND o.statusOrcamentoEntidade.id IN (:enviadoId, :reprovadoId))
+            """)
+    Page<Orcamento> findAprovadosOuPropriosNaoAprovados(
+            @Param("idUsuario") Long idUsuario,
+            @Param("aprovadoId") Integer aprovadoId,
+            @Param("enviadoId") Integer enviadoId,
+            @Param("reprovadoId") Integer reprovadoId,
+            Pageable pageable);
+
+    Page<Orcamento> findBySolicitanteId(Long id, Pageable pageable);
 }
--- src/main/java/com/api/controleverbasbackend/service/OrcamentoService.java
+++ src/main/java/com/api/controleverbasbackend/service/OrcamentoService.java
@@ -1,8 +1,15 @@
 package com.api.controleverbasbackend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.api.controleverbasbackend.domain.orcamento.StatusOrcamentoEnum;
+import com.api.controleverbasbackend.domain.usuario.TipoUsuarioEnum;
+import com.api.controleverbasbackend.domain.usuario.Usuario;
+import com.api.controleverbasbackend.dto.orcamento.DadosListagemOrcamento;
 import com.api.controleverbasbackend.repository.OrcamentoRepository;
 import com.api.controleverbasbackend.repository.StatusOrcamentoRepository;
 
@@ -14,5 +21,30 @@ public class OrcamentoService {
 
     @Autowired
     StatusOrcamentoRepository statusOrcamentoRepository;
+
+    @Transactional
+    public Page<DadosListagemOrcamento> listar(Pageable pageable, Usuario usuarioLogado) {
+        Integer idTipoUsuario = usuarioLogado.getTipoUsuario().getId();
+        Long idUsuario = usuarioLogado.getId();
+
+        Integer ENVIADO = StatusOrcamentoEnum.ENVIADO.getId();
+        Integer APROVADO = StatusOrcamentoEnum.APROVADO.getId();
+        Integer REPROVADO = StatusOrcamentoEnum.REPROVADO.getId();
+
+        if (idTipoUsuario.equals(TipoUsuarioEnum.ADMIN.getId())
+                || idTipoUsuario.equals(TipoUsuarioEnum.GESTOR.getId())) {
+            return orcamentoRepository.findAll(pageable).map(DadosListagemOrcamento::new);
+
+        } else if (idTipoUsuario.equals(TipoUsuarioEnum.TESOUREIRO.getId())) {
+            return orcamentoRepository.findAprovadosOuPropriosNaoAprovados(
+                    idUsuario, APROVADO, ENVIADO, REPROVADO, pageable).map(DadosListagemOrcamento::new);
+
+        } else if (idTipoUsuario.equals(TipoUsuarioEnum.COMUM.getId())) {
+            return orcamentoRepository.findBySolicitanteId(idUsuario, pageable)
+                    .map(DadosListagemOrcamento::new);
+        }
+
+        return Page.empty();
+    }
 
 }
