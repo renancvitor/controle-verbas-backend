cb906aedbdd9aa96aedf1c106110364ee39129f4
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-09 20:15:20 -0300

    feat(orcamento): adiciona listar e cadastrar na controladora

---
+++ src/main/java/com/api/controleverbasbackend/controller/OrcamentoController.java
@@ -0,0 +1,54 @@
+package com.api.controleverbasbackend.controller;
+
+import java.net.URI;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.api.controleverbasbackend.domain.usuario.Usuario;
+import com.api.controleverbasbackend.dto.orcamento.DadosCadastroOrcamento;
+import com.api.controleverbasbackend.dto.orcamento.DadosDetalhamentoOrcamento;
+import com.api.controleverbasbackend.dto.orcamento.DadosListagemOrcamento;
+import com.api.controleverbasbackend.service.OrcamentoService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/orcamentos")
+public class OrcamentoController {
+
+    @Autowired
+    private OrcamentoService orcamentoService;
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemOrcamento>> listar(Optional<Integer> statusId,
+            @PageableDefault(size = 20, sort = ("id")) Pageable pageable,
+            @AuthenticationPrincipal Usuario usuarioLogado) {
+
+        Page<DadosListagemOrcamento> page = orcamentoService.listar(pageable, usuarioLogado, statusId);
+        return ResponseEntity.ok(page);
+    }
+
+    @PostMapping
+    public ResponseEntity<DadosDetalhamentoOrcamento> cadastrar(@RequestBody @Valid DadosCadastroOrcamento dados,
+            UriComponentsBuilder uriComponentsBuilder, @AuthenticationPrincipal Usuario usuario) {
+
+        DadosDetalhamentoOrcamento orcamento = orcamentoService.cadastrar(dados, usuario);
+        URI uri = uriComponentsBuilder.path("/orcamentos/{id}")
+                .buildAndExpand(orcamento.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(orcamento);
+    }
+}
