b01307838dd7dc79100bc64966fc180b1337f7d8
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-08 21:44:35 -0300

    feat(orcamento): adiciona DTO de cadastro

---
+++ src/main/java/com/api/controleverbasbackend/dto/orcamento/DadosCadastroOrcamento.java
@@ -0,0 +1,11 @@
+package com.api.controleverbasbackend.dto.orcamento;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosCadastroOrcamento(@NotBlank(message = "Fornecedor é obrigatório.") String fornecedor,
+        @NotBlank(message = "Descrição é obrigatório.") String descricao,
+        @NotBlank(message = "Forma de pagamento é obrigatório.") String formaPagamento,
+        @NotNull(message = "Valor total é obrigatório.") Double valorTotal,
+        String observacoesGerais) {
+}
