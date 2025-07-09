99533cd0a0436db6889c80e8a23365bf811aaec0
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-08 22:01:44 -0300

    feat(orcamento): adiciona DTO listagem

---
+++ src/main/java/com/api/controleverbasbackend/dto/orcamento/DadosListagemOrcamento.java
@@ -0,0 +1,38 @@
+package com.api.controleverbasbackend.dto.orcamento;
+
+import java.time.LocalDate;
+
+import com.api.controleverbasbackend.domain.orcamento.Orcamento;
+
+public record DadosListagemOrcamento(
+        Long id,
+        String fornecedor,
+        String descricao,
+        String formaPagamento,
+        Double valorTotal,
+        String observacoesGerais,
+        String solicitanteNome,
+        String gestorNome,
+        String tesoureiroNome,
+        String status,
+        LocalDate dataCriacao,
+        LocalDate dataAnalise,
+        LocalDate dataLiberacaoVerba) {
+
+    public DadosListagemOrcamento(Orcamento orcamento) {
+        this(
+                orcamento.getId(),
+                orcamento.getFornecedor(),
+                orcamento.getDescricao(),
+                orcamento.getFormaPagamento(),
+                orcamento.getValorTotal(),
+                orcamento.getObservacoesGerais(),
+                orcamento.getSolicitante().getPessoa().getNome(),
+                orcamento.getGestor() != null ? orcamento.getGestor().getPessoa().getNome() : null,
+                orcamento.getTesoureiro() != null ? orcamento.getTesoureiro().getPessoa().getNome() : null,
+                orcamento.getStatusOrcamentoEntidade().getNome(),
+                orcamento.getDataCriacao(),
+                orcamento.getDataAnalise(),
+                orcamento.getDataLiberacaoVerba());
+    }
+}
