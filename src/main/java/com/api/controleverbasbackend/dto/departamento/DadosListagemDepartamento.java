de228ed0e461da8d8f2cefca55a62327e7730623
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-15 17:50:19 -0300

    feat(departamentos): adiciona à listagem o atributo ativo

--- src/main/java/com/api/controleverbasbackend/dto/departamento/DadosListagemDepartamento.java
+++ src/main/java/com/api/controleverbasbackend/dto/departamento/DadosListagemDepartamento.java
@@ -2,11 +2,12 @@ package com.api.controleverbasbackend.dto.departamento;
 
 import com.api.controleverbasbackend.domain.departamento.Departamento;
 
-public record DadosListagemDepartamento(Long id, String nome) {
+public record DadosListagemDepartamento(Long id, String nome, Boolean ativo) {
 
     public DadosListagemDepartamento(Departamento departamento) {
         this(
                 departamento.getId(),
-                departamento.getNome());
+                departamento.getNome(),
+                departamento.getAtivo());
     }
 }
