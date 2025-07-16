79448f9402b2de9cbb4e765543fae9c96baa85fc
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-16 17:47:42 -0300

    refactor(usuarios): melhora listagem para incluir filtro do status

--- src/main/java/com/api/controleverbasbackend/dto/usuario/DadosListagemUsuario.java
+++ src/main/java/com/api/controleverbasbackend/dto/usuario/DadosListagemUsuario.java
@@ -3,11 +3,12 @@ package com.api.controleverbasbackend.dto.usuario;
 import com.api.controleverbasbackend.domain.usuario.Usuario;
 
 public record DadosListagemUsuario(Long id, String cpf, String email,
-        String TipoUsuario) {
+        String TipoUsuario, Boolean ativo) {
     public DadosListagemUsuario(Usuario usuario) {
         this(usuario.getId(),
                 usuario.getPessoa().getCpf(),
                 usuario.getPessoa().getEmail(),
-                usuario.getTipoUsuario().getNome());
+                usuario.getTipoUsuario().getNome(),
+                usuario.getAtivo());
     }
 }
