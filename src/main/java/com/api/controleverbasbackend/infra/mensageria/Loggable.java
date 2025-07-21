09f5369aa2cfef5415c694ec6389f7f4cc905243
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-21 18:49:44 -0300

    refactor(kafka): melhora funcionalidade para usar múltiplas anotações

--- src/main/java/com/api/controleverbasbackend/infra/mensageria/Loggable.java
+++ src/main/java/com/api/controleverbasbackend/infra/mensageria/Loggable.java
@@ -2,8 +2,11 @@ package com.api.controleverbasbackend.infra.mensageria;
 
 import java.lang.annotation.*;
 
+@Repeatable(Loggables.class)
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Loggable {
-    String tipo() default "INFO";
+    TipoLog tipo(); // Tipo da ação: INSERT, DELETE, etc.
+
+    String entidade(); // Nome da entidade sendo manipulada (ex: "Usuario")
 }
