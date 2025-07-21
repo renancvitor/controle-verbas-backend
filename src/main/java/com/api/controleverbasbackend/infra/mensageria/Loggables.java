081188551c24a91ad19ec2d4a2441848c8f59f9d
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-21 18:48:29 -0300

    feat(kafka): adiciona funcionalidade para permitir múltiplas anotações do tipo Loggable

---
+++ src/main/java/com/api/controleverbasbackend/infra/mensageria/Loggables.java
@@ -0,0 +1,12 @@
+package com.api.controleverbasbackend.infra.mensageria;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Loggables {
+    Loggable[] value();
+}
