223bb551dbbfd778eec4d748491aecb07c9ab8eb
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-07-22 15:21:49 -0300

    refactor(usuario): melhora retorno da DTO para incluir e-mail do usuário

--- src/main/java/com/api/controleverbasbackend/dto/usuario/DadosResumidoUsuario.java
+++ src/main/java/com/api/controleverbasbackend/dto/usuario/DadosResumidoUsuario.java
@@ -2,12 +2,13 @@ package com.api.controleverbasbackend.dto.usuario;
 
 import com.api.controleverbasbackend.domain.usuario.Usuario;
 
-public record DadosResumidoUsuario(Long id, String nomeTipoUsuario, String nomePessoa) {
+public record DadosResumidoUsuario(Long id, String nomeTipoUsuario, String nomePessoa, String email) {
 
     public DadosResumidoUsuario(Usuario usuario) {
         this(
                 usuario.getId(),
                 usuario.getTipoUsuario().getNome(),
-                usuario.getPessoa().getNome());
+                usuario.getPessoa().getNome(),
+                usuario.getPessoa().getEmail());
     }
 }
