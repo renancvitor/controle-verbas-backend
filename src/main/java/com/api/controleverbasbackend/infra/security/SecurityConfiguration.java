53706606c24bef81289f9a9302fc3f4334d786a0
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-08-01 17:38:22 -0300

    feat(security): libera endpoint actuator para deploy

--- src/main/java/com/api/controleverbasbackend/infra/security/SecurityConfiguration.java
+++ src/main/java/com/api/controleverbasbackend/infra/security/SecurityConfiguration.java
@@ -38,6 +38,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.GET, "/<caminho>/**", "/<caminho>").permitAll()
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
