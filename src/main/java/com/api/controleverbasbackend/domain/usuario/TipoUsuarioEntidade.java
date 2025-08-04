9aa0acab656683ca0ecd71b487d8c05486d9072a
Renan C Vitor <renan.vitor.cm@gmail.com>
2025-08-03 21:35:09 -0300

    feat(tipo usuarios): adiciona anotação Setter para testes

--- src/main/java/com/api/controleverbasbackend/domain/usuario/TipoUsuarioEntidade.java
+++ src/main/java/com/api/controleverbasbackend/domain/usuario/TipoUsuarioEntidade.java
@@ -15,9 +15,11 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tipos_usuarios")
+@Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
